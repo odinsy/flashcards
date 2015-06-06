7e95d008ef7db175ed4be7205b80e6ef0134a779
odinsy <odidoit@gmail.com>
2015-06-06 22:15:17 +0300

    create actions and partial

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,52 @@
 class CardsController < ApplicationController
+
+  before_action :find_card, only: [:show, :edit, :update, :destroy]
+
   def index
     @cards = Card.all
   end
+
+  def show
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+  end
+
+  def create
+    @card = Card.create(card_params)
+    if @card.errors.empty?
+      redirect_to @card
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @card.update_attributes(card_params)
+    if @card.errors.empty?
+      redirect_to @card
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
+
+    def find_card
+      @card = Card.find(params[:id])
+    end
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
 end
