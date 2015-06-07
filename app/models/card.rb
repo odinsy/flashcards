5060508e5065f238a02c6380b7eb26e2ff086061
odinsy <odidoit@gmail.com>
2015-06-08 00:06:22 +0300

    Add validates and callback

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,15 @@ class Card < ActiveRecord::Base
   validates :original_text, presence: true
   validates :translated_text, presence: true
   validates :review_date, presence: true
+  validate  :check_text
+
+  before_validation do
+    self.review_date = (self.review_date + 3)
+  end
+  
+  def check_text
+    if self.original_text.downcase.strip == self.translated_text.downcase.strip
+      puts errors.add(:original_text, "Text can't be the same")
+    end
+  end
 end
