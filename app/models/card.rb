5b4d23bca66fce36da945c7c3a65a7cbab3a0042
odinsy <odidoit@gmail.com>
2015-06-08 10:43:49 +0300

    modify callback and partial _form

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,14 +3,18 @@ class Card < ActiveRecord::Base
   validates :translated_text, presence: true
   validates :review_date, presence: true
   validate  :check_text
+  before_validation :set_review_date, on: :create
 
-  before_validation do
-    self.review_date = (self.review_date + 3)
-  end
-  
+  private
+
   def check_text
     if self.original_text.downcase.strip == self.translated_text.downcase.strip
-      puts errors.add(:original_text, "Text can't be the same")
+      errors.add(:original_text, "Text can't be the same")
     end
   end
+
+  def set_review_date
+    self.review_date = (self.review_date + 3)
+  end
+
 end
