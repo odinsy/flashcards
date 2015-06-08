aec50dcf60a39eac9e2f4c58d3f9335b0538d6f9
odinsy <odidoit@gmail.com>
2015-06-08 14:35:19 +0300

    refactoring

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,20 +1,18 @@
 class Card < ActiveRecord::Base
-  validates :original_text, presence: true
-  validates :translated_text, presence: true
-  validates :review_date, presence: true
-  validate  :check_text
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate  :texts_are_different
   before_validation :set_review_date, on: :create
 
   private
 
-  def check_text
+  def texts_are_different
     if self.original_text.downcase.strip == self.translated_text.downcase.strip
       errors.add(:original_text, "Text can't be the same")
     end
   end
 
   def set_review_date
-    self.review_date = (self.review_date + 3)
+    self.review_date += 3.days
   end
 
 end
